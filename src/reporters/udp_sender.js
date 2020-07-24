3c94d65a87036775e74b56b7e746a86a2ede55e4
Lukas Welinder <lukas@welinder.co>
2020-07-24 09:54:12 -0700

    feat: support browser

--- src/reporters/udp_sender.js
+++ src/reporters/udp_sender.js
@@ -56,7 +56,94 @@ export default class UDPSender {
       allowFilesystemAccess: true,
     });
     this._jaegerThrift = new Thrift({
-      source: fs.readFileSync(path.join(__dirname, '../jaeger-idl/thrift/jaeger.thrift'), 'ascii'),
+      source:
+        '# Copyright (c) 2016 Uber Technologies, Inc.\n' +
+        '#\n' +
+        '# Permission is hereby granted, free of charge, to any person obtaining a copy\n' +
+        '# of this software and associated documentation files (the "Software"), to deal\n' +
+        '# in the Software without restriction, including without limitation the rights\n' +
+        '# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n' +
+        '# copies of the Software, and to permit persons to whom the Software is\n' +
+        '# furnished to do so, subject to the following conditions:\n' +
+        '#\n' +
+        '# The above copyright notice and this permission notice shall be included in\n' +
+        '# all copies or substantial portions of the Software.\n' +
+        '#\n' +
+        '# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n' +
+        '# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n' +
+        '# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n' +
+        '# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n' +
+        '# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n' +
+        '# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n' +
+        '# THE SOFTWARE.\n' +
+        '\n' +
+        'namespace java com.uber.jaeger.thriftjava\n' +
+        '\n' +
+        "# TagType denotes the type of a Tag's value.\n" +
+        'enum TagType { STRING, DOUBLE, BOOL, LONG, BINARY }\n' +
+        '\n' +
+        '# Tag is a basic strongly typed key/value pair. It has been flattened to reduce the use of pointers in golang\n' +
+        'struct Tag {\n' +
+        '  1: required string  key\n' +
+        '  2: required TagType vType\n' +
+        '  3: optional string  vStr\n' +
+        '  4: optional double  vDouble\n' +
+        '  5: optional bool    vBool\n' +
+        '  6: optional i64     vLong\n' +
+        '  7: optional binary  vBinary\n' +
+        '}\n' +
+        '\n' +
+        '# Log is a timed even with an arbitrary set of tags.\n' +
+        'struct Log {\n' +
+        '  1: required i64       timestamp\n' +
+        '  2: required list<Tag> fields\n' +
+        '}\n' +
+        '\n' +
+        'enum SpanRefType { CHILD_OF, FOLLOWS_FROM }\n' +
+        '\n' +
+        "# SpanRef describes causal relationship of the current span to another span (e.g. 'child-of')\n" +
+        'struct SpanRef {\n' +
+        '  1: required SpanRefType refType\n' +
+        '  2: required i64         traceIdLow\n' +
+        '  3: required i64         traceIdHigh\n' +
+        '  4: required i64         spanId\n' +
+        '}\n' +
+        '\n' +
+        '# Span represents a named unit of work performed by a service.\n' +
+        'struct Span {\n' +
+        '  1:  required i64           traceIdLow   # the least significant 64 bits of a traceID\n' +
+        '  2:  required i64           traceIdHigh  # the most significant 64 bits of a traceID; 0 when only 64bit IDs are used\n' +
+        '  3:  required i64           spanId       # unique span id (only unique within a given trace)\n' +
+        '  4:  required i64           parentSpanId # since nearly all spans will have parents spans, CHILD_OF refs do not have to be explicit\n' +
+        '  5:  required string        operationName\n' +
+        '  6:  optional list<SpanRef> references   # causal references to other spans\n' +
+        '  7:  required i32           flags        # a bit field used to propagate sampling decisions. 1 signifies a SAMPLED span, 2 signifies a DEBUG span.\n' +
+        '  8:  required i64           startTime\n' +
+        '  9:  required i64           duration\n' +
+        '  10: optional list<Tag>     tags\n' +
+        '  11: optional list<Log>     logs\n' +
+        '}\n' +
+        '\n' +
+        '# Process describes the traced process/service that emits spans.\n' +
+        'struct Process {\n' +
+        '  1: required string    serviceName\n' +
+        '  2: optional list<Tag> tags\n' +
+        '}\n' +
+        '\n' +
+        '# Batch is a collection of spans reported out of process.\n' +
+        'struct Batch {\n' +
+        '  1: required Process    process\n' +
+        '  2: required list<Span> spans\n' +
+        '}\n' +
+        '\n' +
+        '# BatchSubmitResponse is the response on submitting a batch. \n' +
+        'struct BatchSubmitResponse {\n' +
+        "    1: required bool ok   # The Collector's client is expected to only log (or emit a counter) when not ok equals false\n" +
+        '}\n' +
+        '\n' +
+        'service Collector  {\n' +
+        '    list<BatchSubmitResponse> submitBatches(1: list<Batch> batches)\n' +
+        '}\n',
       allowOptionalArguments: true,
     });
     this._totalSpanBytes = 0;
